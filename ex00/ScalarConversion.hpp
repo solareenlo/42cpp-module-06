ca6335fc780903e4cc0b8fc2ac19fdbb13a96101
solareenlo <solareenlo@gmail.com>
2021-06-07 02:12:57 +0900

    update: ex00/ScalarConversion.hpp

--- ex00/ScalarConversion.hpp
+++ ex00/ScalarConversion.hpp
@@ -6,14 +6,30 @@
 /*   By: tayamamo <tayamamo@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/06 14:00:47 by tayamamo          #+#    #+#             */
-/*   Updated: 2021/06/06 14:01:19 by tayamamo         ###   ########.fr       */
+/*   Updated: 2021/06/07 02:12:13 by tayamamo         ###   ########.fr       */
 /*   Copyright 2021                                                           */
 /* ************************************************************************** */
 
 #ifndef EX00_SCALARCONVERSION_HPP_
 #define EX00_SCALARCONVERSION_HPP_
 
+#include <string>
+
 class   ScalarConversion {
+ public:
+    explicit ScalarConversion(std::string const& str);
+    ScalarConversion(ScalarConversion const& src);
+    ~ScalarConversion();
+    ScalarConversion&   operator=(ScalarConversion const& right);
+
+    char    toChar();
+    int     toInt();
+    float   toFloat();
+    double  toDboule();
+
+ private:
+    ScalarConversion();
+    std::string const& str;
 };
 
 #endif  // EX00_SCALARCONVERSION_HPP_
